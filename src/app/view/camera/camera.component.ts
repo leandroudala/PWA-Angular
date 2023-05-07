aefe2e6ec9a475392d15634706d98e204bd21978
Leandro Udala <leandro.correia@radixeng.com.br>
2023-05-07 15:02:09 -0300

    Added camera

--- src/app/view/camera/camera.component.ts
+++ src/app/view/camera/camera.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { BarcodeFormat } from '@zxing/library';
+import { ZXingScannerComponent } from "@zxing/ngx-scanner";
 
 @Component({
   selector: 'app-camera',
@@ -8,9 +9,66 @@ import { BarcodeFormat } from '@zxing/library';
 })
 export class CameraComponent implements OnInit {
 
+  @ViewChild("scanner", {static: false}) scanner: ZXingScannerComponent;
+
+  autostart = false;
+
+  deviceCurrent: MediaDeviceInfo;
+
   constructor() { }
 
   ngOnInit(): void {
+    console.log("Started");
+  }
+
+  scannedValue = "";
+  availableDevices: MediaDeviceInfo[] = [];
+  hasDevices = false;
+  hasPermission = false;
+
+  async startCamera() {
+    if (!this.hasPermission) {
+      this.hasPermission = await this.scanner.askForPermission();
+      if (!this.hasPermission) {
+        throw Error("No Permission");
+      } else {
+        console.log("Permission granted");
+      }
+    }
+
+    if (!this.hasDevices) {
+      throw Error("No devices.");
+    }
+    
+    if (this.deviceCurrent == null) {
+      this.selectFirstDevice();
+    } else {
+      this.scanner.scanStart();
+    }
+    
+  }
+
+  selectFirstDevice() {
+    this.deviceCurrent = this.availableDevices[0];
+  }
+
+  onCamerasFound(devices: MediaDeviceInfo[]) {
+    this.availableDevices = devices;
+    this.hasDevices = Boolean(devices && devices.length);
+
+    if (!this.hasDevices) {
+      alert("No camera device found");
+    }
+
+  }
+
+  onHasPermission(hasPermission: boolean) {
+    this.hasPermission = hasPermission;
+  }
+
+  onScan(value: string) {
+    this.scannedValue = value;
+    this.scanner.scanStop()
   }
 
   allowedFormats = [ BarcodeFormat.QR_CODE ]
